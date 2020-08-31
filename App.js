fc9fbb538eaf9c11cdc6af92841083d3e0d467ee
dpoole31 <dpoole31@gatech.edu>
2020-08-30 20:24:45 -0400

    initialized native-base

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import React, { Component } from 'react';
+import { Container, Content, Text, StyleProvider } from 'native-base';
+import getTheme from './native-base-theme/components';
+import platform from "./native-base-theme/variables/platform";
+export default class App extends Component {
+  render() {
+    return (
+        <StyleProvider style={getTheme(platform)}>
+          <Container>
+            <Content>
+              <Text>
+                Hello, World!
+              </Text>
+            </Content>
+          </Container>
+        </StyleProvider>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
